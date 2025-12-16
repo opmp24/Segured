9846a654f16f793282bb850e2d8fa0f7975fef57
Demo User <you@example.com>
2025-12-16 18:43:13 -0300

    youtube y galeria

--- drive-config.js
+++ drive-config.js
@@ -2,5 +2,12 @@
 
 window.DRIVE_CONFIG = {
   // ID de la carpeta de Google Drive que contiene los documentos públicos.
-  documentsFolderId: "1nPjNv26V6m1qYe4p3zKWHd7RCNdD02Mc"
+  documentsFolderId: "1nPjNv26V6m1qYe4p3zKWHd7RCNdD02Mc",
+
+  // ID de la carpeta de Google Drive que contiene las imágenes para la galería.
+  galleryFolderId: "1YglcilvObqlV2G7lJwWJLlMFuRYLrXGH",
+
+  // ID del canal de YouTube del que se mostrará el último video.
+  // Reemplaza "YOUR_YOUTUBE_CHANNEL_ID" con el ID de tu canal.
+  youtubeChannelId: "UCVwlW3kCGE0DTtX95_I7GtA"
 };
